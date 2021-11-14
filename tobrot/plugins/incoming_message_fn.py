f11f133b2bc717df1885b4bc7d3d7bb478cb5ee9
vijayadithya330 <88371471+vijayadithya330@users.noreply.github.com>
2021-11-14 08:38:51 +0530

    Update incoming_message_fn.py

--- tobrot/plugins/incoming_message_fn.py
+++ tobrot/plugins/incoming_message_fn.py
@@ -101,7 +101,7 @@ async def incoming_message_f(client, message):
             aria_i_p = await aria_start()
             # LOGGER.info(aria_i_p)
 
-        await i_m_sefg.edit_text("")
+        await i_m_sefg.edit_text("...")
         # try to download the "link"
         is_zip = False
         is_cloud = False
