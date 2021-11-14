04bef7cc0c7bb148759b13766fd77b046bf27344
vijayadithya330 <88371471+vijayadithya330@users.noreply.github.com>
2021-11-14 08:57:51 +0530

    Update download_aria_p_n.py

--- tobrot/helper_funcs/download_aria_p_n.py
+++ tobrot/helper_funcs/download_aria_p_n.py
@@ -327,6 +327,7 @@ async def check_progress_for_dl(aria2, gid, event, previous_message):
                 msg += f"\n<b>👀Status</b>: {file.progress_string()} <b>Of</b> {file.total_length_string()}"
                 msg += f"\n<b>🕒ETA:</b> {file.eta_string()}"
                 msg += f"\n{msgg}"
+                msg += f"\n<b>Using Engine:</b> Bharat Torrent"
                 msg += f"\n"
                 inline_keyboard = []
                 ikeyboard = []
